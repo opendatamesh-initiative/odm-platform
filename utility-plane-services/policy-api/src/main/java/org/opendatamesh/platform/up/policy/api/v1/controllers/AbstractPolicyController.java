10a4334066b1ac3304293a2098af55efe791ac3b
Mattia Pennati <mattia.pennati@quantyca.it>
2023-07-04 18:57:13 +0200

    clean dependencies for policy-api module

--- utility-plane-services/policy-api/src/main/java/org/opendatamesh/platform/up/policy/api/v1/controllers/AbstractPolicyController.java
+++ utility-plane-services/policy-api/src/main/java/org/opendatamesh/platform/up/policy/api/v1/controllers/AbstractPolicyController.java
@@ -14,11 +14,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 
 import javax.validation.Valid;
-import java.net.ConnectException;
 
 @RestController
 @RequestMapping(

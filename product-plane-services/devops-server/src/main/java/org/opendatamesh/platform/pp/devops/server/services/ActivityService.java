8299aee2701b466b3c3c8fc625791e3fc000dc7c
Andrea Gioia <andrea.gioia@quantyca.it>
2023-07-08 00:03:23 +0200

    Refactored modules structure

--- product-plane-services/devops-server/src/main/java/org/opendatamesh/platform/pp/devops/server/services/ActivityService.java
+++ product-plane-services/devops-server/src/main/java/org/opendatamesh/platform/pp/devops/server/services/ActivityService.java
@@ -2,34 +2,51 @@ package org.opendatamesh.platform.pp.devops.server.services;
 
 
 
+import java.util.List;
+
+import org.opendatamesh.platform.core.dpds.api.ApiParser;
 import org.opendatamesh.platform.core.dpds.model.DataProductVersionDPDS;
+import org.opendatamesh.platform.core.dpds.model.InfrastructuralComponentDPDS;
+import org.opendatamesh.platform.core.dpds.model.ProvisionInfoDPDS;
 import org.opendatamesh.platform.pp.devops.api.resources.ActivityResource;
 import org.opendatamesh.platform.pp.devops.server.database.entities.Activity;
 import org.opendatamesh.platform.pp.registry.api.v1.clients.RegistryClient;
 import org.opendatamesh.platform.up.executor.api.clients.ExecutorClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.opendatamesh.platform.up.executor.api.resources.TaskResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityService {
 
-    @Value("${odm.utilityPlane.executorServices.azureDevOps.address}")
-    private String registryAddress;
-
     ExecutorClient executor;
+
+    @Autowired
     RegistryClient registry;
 
-    public ActivityService() {
-        registry = new RegistryClient(registryAddress);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(ActivityService.class);
+
+    public ActivityService() { }
 
     public ActivityResource createActivity(Activity activity,
             boolean startAfterCreation) {
 
         DataProductVersionDPDS dataProductVersion = registry.readDataProductVersion(Long.parseLong(activity.getDataProductId()), activity.getDataProductVersion());
         
-        dataProductVersion.getInternalComponents().getApplicationComponents();
-        executor = new ExecutorClient(null);
+        List<InfrastructuralComponentDPDS> infraCompnents = dataProductVersion.getInternalComponents().getInfrastructuralComponents();
+        InfrastructuralComponentDPDS infraComponent = infraCompnents.get(0);
+        ProvisionInfoDPDS provisionInfo =  infraComponent.getProvisionInfo();
+        String serviceRef = provisionInfo.getService().getRef();
+        if(serviceRef.equalsIgnoreCase("azuredevops")) {
+            executor = new ExecutorClient("http://localhost:8482");
+            TaskResource task = new TaskResource();
+            task.setTemplate(provisionInfo.getTemplate().getRawContent());
+            //Stask.setTemplate(provisionInfo.getConfigurations());
+        } else {
+            throw new RuntimeException("Not supported");
+        }
         return null;
     }
    

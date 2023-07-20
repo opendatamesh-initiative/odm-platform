5aef2dfa003fbb8f9d18502cdb087be356cac945
Mattia Pennati <mattia.pennati@quantyca.it>
2023-07-20 16:38:06 +0200

    First implementation of NotificationClient + move Error resource from server to api module

--- utility-plane-services/notification-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/exceptions/AppExceptionHandler.java
+++ utility-plane-services/notification-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/exceptions/AppExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.opendatamesh.platform.up.metaservice.server.exceptions;
 
+import org.opendatamesh.platform.up.notification.api.resources.Error;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;

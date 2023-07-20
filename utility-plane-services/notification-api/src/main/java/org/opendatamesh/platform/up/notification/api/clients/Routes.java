666fb98f5af70a9d23fa643ce894aabc378a502f
Mattia Pennati <mattia.pennati@quantyca.it>
2023-07-20 16:50:41 +0200

    First implementation of NotificationClient + move Error resource from server to api module

---
+++ utility-plane-services/notification-api/src/main/java/org/opendatamesh/platform/up/notification/api/clients/Routes.java
@@ -0,0 +1,23 @@
+package org.opendatamesh.platform.up.notification.api.clients;
+
+import org.opendatamesh.platform.core.commons.clients.RoutesInterface;
+
+public enum Routes implements RoutesInterface {
+
+    METASERVICE_NOTIFICATION("/api/v1/up/metaservice/");
+
+    private final String path;
+
+    Routes(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return this.path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}

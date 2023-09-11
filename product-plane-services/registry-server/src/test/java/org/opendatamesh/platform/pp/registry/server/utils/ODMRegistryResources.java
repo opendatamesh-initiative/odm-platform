0d17c2bdd455a1a88759d9d247f66b193dfd8865
Andrea Gioia <andrea.gioia@quantyca.it>
2023-09-12 01:36:33 +0200

    Created abstract controllers for all services exposed by registry module

--- product-plane-services/registry-server/src/test/java/org/opendatamesh/platform/pp/registry/server/utils/ODMRegistryResources.java
+++ product-plane-services/registry-server/src/test/java/org/opendatamesh/platform/pp/registry/server/utils/ODMRegistryResources.java
@@ -14,24 +14,7 @@ public enum ODMRegistryResources {
     DPD_CORE_WITH_INTERNAL_REF("dpd-references/dpd-core-internal/dpd-core.json"),
     DPD_CORE_WITH_MIX_REF("dpd-references/dpd-core-mix/dpd-core.json"),
 
-    
-    // DATA PRDODUCTS
-    /* 
-    DP1("test/dataproduct-descriptor/dp1.json"),
-    RESOURCE_DP1_UPD("test/dataproduct-descriptor/dp1-updated.json"),
-    */
-    RESOURCE_DP1_V1("test/dataproduct-descriptor/dp1-v1.json"),
-
-    //public static final String  RESOURCE_DP1_V1_API1 = "src/test/resources/test/dataproduct-descriptor/dp1-v1-api1.json";
-    /* 
-    API_DEF1_V1("test/definition/def1.json"),
-    API_DEF1_NOVERSION("test/definition/def1-missing-version.json"),
-    API_DEF1_NONAME("test/definition/def1-missing-name.json"),
-    API_DEF1_NONAME_NOVERSION("test/definition/def2-missing-name-version.json"),
-    */
-
-    RESOURCE_TEMPLATE_1("test/template/template1.json"),
-    RESOURCE_TEMPLATE_2("test/template/template2.json"),
+     
     RESOURCE_SCHEMA1("test/schema/schema1.json"),
     
     RESOURCE_DPS_URI("https://raw.githubusercontent.com/opendatamesh-initiative/odm-specification-dpdescriptor/main/examples/tripexecution/data-product-descriptor.json");

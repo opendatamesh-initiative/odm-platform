ad4f4d5dd42b847e0a921e24da9ae327b2e8d8f1
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2023-10-13 14:57:56 +0200

    first draft az pipeline

---
+++ deployScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update ; sudo apt-get install dialog apt-utils -y; sudo apt-get install openjdk-17-jdk openjdk-17-jre -y
+
+#mkdir -p /home/$USER/src/main/resources/db/migration && cp /home/$USER/data.csv /home/$USER/src/main/resources/db/migration/data.csv
+
+nohup java -jar /home/$USER/odm-platform-pp-registry-server-1.0.0.jar > /home/$USER/odm-platform-pp-registry-server-1.0.0.log 2>&1 &
+
+nohup java -jar /home/$USER/odm-platform-pp-devops-server-1.0.0.jar > /home/$USER/odm-platform-pp-devops-server-1.0.0.log 2>&1 &
+
+echo Script lanciato

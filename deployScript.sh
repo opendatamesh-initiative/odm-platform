f92aa1255d3e0d50a42f2047d6b9bbb932867c81
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2023-10-17 13:13:18 +0200

    fix error script

--- deployScript.sh
+++ deployScript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update ; sudo apt-get install dialog apt-utils -y; sudo apt-get install openjdk-17-jdk openjdk-17-jre -y
 
 #mkdir -p /home/$USER/src/main/resources/db/migration && cp /home/$USER/data.csv /home/$USER/src/main/resources/db/migration/data.csv

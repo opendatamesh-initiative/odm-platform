75cdd68267753277f71d371abdaea6dd111374b0
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2023-10-19 12:45:57 +0200

    fix script

--- deployScript.sh
+++ deployScript.sh
@@ -2,6 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
 sudo apt-get update ; sudo apt-get install dialog apt-utils -y; sudo apt-get install openjdk-17-jdk openjdk-17-jre -y
 
 #mkdir -p /home/$USER/src/main/resources/db/migration && cp /home/$USER/data.csv /home/$USER/src/main/resources/db/migration/data.csv

f7cc2d3ddaad7d232d6636a86f98c1e75c1158d9
Mattia Pennati <mattia.pennati@quantyca.it>
2024-02-08 19:09:02 +0100

    Add extra configurations for Apache Velocity to blueprint-server

--- product-plane-services/blueprint-server/src/main/java/org/opendatamesh/platform/pp/blueprint/server/configs/VelocityConfig.java
+++ product-plane-services/blueprint-server/src/main/java/org/opendatamesh/platform/pp/blueprint/server/configs/VelocityConfig.java
@@ -13,11 +13,23 @@ public class VelocityConfig {
     @Value("${git.templates.path}")
     private String velocityTemplatePath;
 
+    @Value("${git.templates.variable.delimiter.start}")
+    private String velocityVariableDelimiterStart;
+
+    @Value("${git.templates.variable.delimiter.stop}")
+    private String velocityVariableDelimiterStop;
+
+    @Value("${git.templates.variable.evaluation.strict}")
+    private String velocityStrictEvaluation;
+
     @Bean
     public VelocityEngine velocityEngine() {
         VelocityEngine velocityEngine = new VelocityEngine();
         Properties velocityProperties = new Properties();
         velocityProperties.setProperty("resource.loader.file.path", velocityTemplatePath + "/projects");
+        velocityProperties.setProperty("runtime.interpolate.string", velocityStrictEvaluation);
+        velocityProperties.setProperty("runtime.interpolate.stopstring", velocityStrictEvaluation);
+        velocityProperties.setProperty("runtime.reference.strict", velocityStrictEvaluation);
         velocityEngine.init(velocityProperties);
         return velocityEngine;
     }

854719cebcb965165759c374ae5f449d3caead10
Mattia Pennati <mattia.pennati@quantyca.it>
2024-02-23 12:12:15 +0100

    Minor fixes resources

--- product-plane-services/policy-api/src/main/java/org/opendatamesh/platform/pp/policy/api/resources/PolicyResource.java
+++ product-plane-services/policy-api/src/main/java/org/opendatamesh/platform/pp/policy/api/resources/PolicyResource.java
@@ -1,4 +1,9 @@
 package org.opendatamesh.platform.pp.policy.api.resources;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PolicyResource {
 }

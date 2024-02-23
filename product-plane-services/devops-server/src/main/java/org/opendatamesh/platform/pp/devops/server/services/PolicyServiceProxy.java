a670d1204b9bbfef141ea1a987b12cba1da4bd4e
Mattia Pennati <mattia.pennati@quantyca.it>
2024-02-23 12:26:36 +0100

    Minor fixes

--- product-plane-services/devops-server/src/main/java/org/opendatamesh/platform/pp/devops/server/services/PolicyServiceProxy.java
+++ product-plane-services/devops-server/src/main/java/org/opendatamesh/platform/pp/devops/server/services/PolicyServiceProxy.java
@@ -11,7 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PolicyServiceProxy extends PolicyClient {
 
     private Boolean policyServiceActive;

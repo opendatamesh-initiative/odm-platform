ea18689d13aa1d953168a48ad8caa9fe3ad74543
Mattia Pennati <mattia.pennati@quantyca.it>
2024-03-01 14:36:29 +0100

    add mock validation IT

--- product-plane-services/policy-server/src/test/java/org/opendatamesh/platform/pp/policy/server/ValidationIT.java
+++ product-plane-services/policy-server/src/test/java/org/opendatamesh/platform/pp/policy/server/ValidationIT.java
@@ -1,7 +1,35 @@
 package org.opendatamesh.platform.pp.policy.server;
 
-public class ValidationIT {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.opendatamesh.platform.pp.policy.api.resources.PolicyEvaluationResultResource;
+import org.opendatamesh.platform.up.notification.api.resources.EventResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
-    // TODO
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidationIT extends ODMPolicyIT {
+
+    // ======================================================================================
+    // VALIDATE Object
+    // ======================================================================================
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void testCreatePolicyAllProperties() throws JsonProcessingException {
+
+        // Resources
+        EventResource mockEventResource = new EventResource();
+        ResponseEntity<PolicyEvaluationResultResource> postResponse =
+                policyClient.validateObject(mockEventResource);
+        PolicyEvaluationResultResource policyEvaluationResultResource = postResponse.getBody();
+
+        // Verification
+        verifyResponseEntity(postResponse, HttpStatus.OK,true);
+        assertThat(policyEvaluationResultResource).isNotNull();
+
+    }
 
 }

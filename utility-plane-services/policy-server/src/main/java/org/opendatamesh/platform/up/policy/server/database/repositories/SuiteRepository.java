6fec986f9bac9c9e89212fd3119367674e916151
giulia.busnelli <giulia.busnelli@blindata.io>
2024-03-22 09:31:03 +0100

    Refactor policy engine

--- utility-plane-services/policy-server/src/main/java/org/opendatamesh/platform/up/policy/server/database/repositories/SuiteRepository.java
+++
@@ -1,9 +0,0 @@
-package org.opendatamesh.platform.up.policy.server.database.repositories;
-
-import org.opendatamesh.platform.up.policy.server.database.entities.SuiteEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SuiteRepository extends PagingAndSortingRepository<SuiteEntity, String> {
-}

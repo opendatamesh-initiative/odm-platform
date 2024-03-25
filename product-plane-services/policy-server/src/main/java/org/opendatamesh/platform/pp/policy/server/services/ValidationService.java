7daca854cc5500153fa0f59af6937864c4e0ce5a
Mattia Pennati <mattia.pennati@quantyca.it>
2024-03-25 17:43:55 +0100

    First draft of SpEL usage

--- product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/ValidationService.java
+++ product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/ValidationService.java
@@ -1,5 +1,6 @@
 package org.opendatamesh.platform.pp.policy.server.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.opendatamesh.platform.core.dpds.ObjectMapperFactory;
@@ -11,10 +12,13 @@ import org.opendatamesh.platform.pp.policy.server.services.validation.PolicyDisp
 import org.opendatamesh.platform.pp.policy.server.services.validation.PolicyEnricherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ValidationService {
@@ -46,13 +50,16 @@ public class ValidationService {
         ValidationResponseResource response = new ValidationResponseResource();
         List<PolicyEvaluationResultResource> policyResults = new ArrayList<>();
         Boolean validationResult = true;
-
-        // Fetch policy to evaluate
-        List<Policy> policiesToEvaluate = selectPoliciesBySuite(policyEvaluationRequestResource.getEvent());
-
-        // Evaluate policies and update response
         PolicyEvaluationResultResource basePolicyResult = initBasePolicyEvaluationResult(policyEvaluationRequestResource);
         PolicyEvaluationResultResource policyResult;
+
+        // Fetch policy to evaluate
+        List<Policy> policiesToEvaluate = selectPolicies(
+                policyEvaluationRequestResource.getEvent(),
+                basePolicyResult.getInputObject()
+        );
+
+        // Evaluate policies and update response
         for (Policy policyToEvaluate : policiesToEvaluate) {
             basePolicyResult.setPolicyId(policyToEvaluate.getId());
             policyResult = validateInputSinglePolicy(
@@ -85,6 +92,15 @@ public class ValidationService {
     // Policy Selector Method
     // ======================================================================================
 
+    private List<Policy> selectPolicies(
+            PolicyEvaluationRequestResource.EventType suite,
+            JsonNode inputObject
+    ) {
+        List<Policy> policySubset = selectPoliciesBySuite(suite);
+        policySubset = selectPoliciesBySpELExpression(policySubset, inputObject);
+        return policySubset;
+    }
+
     private List<Policy> selectPoliciesBySuite(PolicyEvaluationRequestResource.EventType suite) {
 
         PolicySearchOptions policySearchOptions = new PolicySearchOptions();
@@ -92,6 +108,31 @@ public class ValidationService {
 
         return policyService.findAllFiltered(Pageable.unpaged(), policySearchOptions).getContent();
 
+    }
+
+    private List<Policy> selectPoliciesBySpELExpression(List<Policy> policies, JsonNode inputObject) {
+
+        // Test expression
+        String testSpELExpression = "['afterState']['domain'] == 'sampleDomain' and ['afterState']['domain'] != 'sampleDomainTwo'";
+        // Parse SpEL expression
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression(testSpELExpression);
+        // Create context
+        Map<String, Object> context = convertJsonNodeToMap(inputObject);
+
+        // Create a new list to store filtered policies
+        List<Policy> filteredPolicies = new ArrayList<>();
+
+        // Iterate over policies and filter them based on the SpEL expression
+        for (Policy policy : policies) {
+            // Evaluate SpEL expression for each policy
+            boolean result = expression.getValue(context, Boolean.class);
+            if (result) {
+                filteredPolicies.add(policy); // Add the policy if the expression evaluates to true
+            }
+        }
+
+        return filteredPolicies;
     }
 
 
@@ -114,6 +155,21 @@ public class ValidationService {
 
         return policyEvaluationResultResource;
 
+    }
+
+    public static Map<String, Object> convertJsonNodeToMap(JsonNode jsonNode) {
+        Map<String, Object> map = new HashMap<>();
+        Iterator<String> fieldNames = jsonNode.fieldNames();
+        while (fieldNames.hasNext()) {
+            String fieldName = fieldNames.next();
+            JsonNode fieldValue = jsonNode.get(fieldName);
+            if (fieldValue.isObject()) {
+                map.put(fieldName, convertJsonNodeToMap(fieldValue));
+            } else {
+                map.put(fieldName, fieldValue.asText());
+            }
+        }
+        return map;
     }
 
 }

fe8d6f9a352b4458c3977dbd5665234f7819bc4b
Mattia Pennati <mattia.pennati@quantyca.it>
2024-03-28 14:10:16 +0100

    minor fixes

--- product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/policy/engine/PolicyEngineProxyImpl.java
+++ product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/policy/engine/PolicyEngineProxyImpl.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("!test & !testmysql & !testpostgresql")
 public class PolicyEngineProxyImpl extends AbstractPolicyEngineProxy {
 
     @Override
--- product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/policy/engine/PolicyEngineProxyMock.java
+++ product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/policy/engine/PolicyEngineProxyMock.java
@@ -3,11 +3,13 @@ package org.opendatamesh.platform.pp.policy.server.services.proxies.policy.engin
 import org.opendatamesh.platform.pp.policy.server.database.entities.PolicyEngine;
 import org.opendatamesh.platform.up.policy.api.v1.clients.PolicyEngineClient;
 import org.opendatamesh.platform.up.policy.api.v1.clients.PolicyEngineClientMock;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile({"test", "testmysql", "testpostgresql"})
+@Primary
 public class PolicyEngineProxyMock extends AbstractPolicyEngineProxy {
 
     @Override

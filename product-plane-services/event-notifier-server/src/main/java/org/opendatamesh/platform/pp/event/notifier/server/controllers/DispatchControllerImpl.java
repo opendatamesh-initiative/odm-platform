8d9f8533b245c40ddbbc5413c6784b3af2aad082
Mattia Pennati <mattia.pennati@quantyca.it>
2024-04-10 08:42:39 +0200

    Minor fixes

--- product-plane-services/event-notifier-server/src/main/java/org/opendatamesh/platform/pp/event/notifier/server/controllers/DispatchControllerImpl.java
+++ product-plane-services/event-notifier-server/src/main/java/org/opendatamesh/platform/pp/event/notifier/server/controllers/DispatchControllerImpl.java
@@ -1,5 +1,6 @@
 package org.opendatamesh.platform.pp.event.notifier.server.controllers;
 
+import org.opendatamesh.platform.pp.event.notifier.api.controllers.AbstractDispatchController;
 import org.opendatamesh.platform.pp.event.notifier.server.services.DispatchService;
 import org.opendatamesh.platform.pp.event.notifier.api.controllers.DispatchController;
 import org.opendatamesh.platform.up.notification.api.resources.EventResource;
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DispatchControllerImpl implements DispatchController {
+public class DispatchControllerImpl extends AbstractDispatchController {
 
     @Autowired
     DispatchService dispatchService;

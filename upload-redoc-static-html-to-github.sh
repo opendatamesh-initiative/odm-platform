ef528e08b59d9b959638814d29468b10392172de
Mattia Pennati <mattia.pennati@quantyca.it>
2024-04-17 16:52:49 +0200

    Override or create subdirectory  and add docs inside it

--- upload-redoc-static-html-to-github.sh
+++ upload-redoc-static-html-to-github.sh
@@ -6,20 +6,21 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-DOC_REPO_URL="git@github.com:opendatamesh-initiative/odm-api-doc.git"
-DOC_REPO_NAME="odm-api-doc"
+DOC_REPO_URL="git@github.com:opendatamesh-initiative/odm-site-platform.git"
+DOC_DIR="odm-site-platform/docs/assets/redoc/doc"
+SCRIPT_DIR="docs/assets/redoc/scripts"
 VERSION="$1"
 
 # Clone doc repository
 git clone $DOC_REPO_URL
-cd $DOC_REPO_NAME
+cd $DOC_DIR
 
 if [ -d "$VERSION" ]; then
 
   echo "Subdirectory $VERSION exists. Overriding its content..."
 
   # Move files to the subdirectory
-  cp ../redocly-docs/* $VERSION
+  cp ../../../../../redocly-docs/* $VERSION
 
 else
 
@@ -29,12 +30,14 @@ else
   mkdir $VERSION
 
   # Move files to the subdirectory
-  cp ../redocly-docs/* $VERSION
+  cp ../../../../../redocly-docs/* $VERSION
 
 fi
 
 # Generate index.html for github pages
-./ghpagesgenerator.sh
+cd ../../../../../
+pwd
+./$SCRIPT_DIR/updateDocPage.sh
 
 # Add changes to the staging area
 git add .
@@ -43,8 +46,8 @@ git add .
 git commit -m "Override or create subdirectory $DIRECTORY and add docs inside it"
 
 # Push changes
-git push origin master
+git push origin main
 
 # Clean up
 cd ..
-rm -rf $DOC_REPO_NAME
+rm -rf $DOC_DIR

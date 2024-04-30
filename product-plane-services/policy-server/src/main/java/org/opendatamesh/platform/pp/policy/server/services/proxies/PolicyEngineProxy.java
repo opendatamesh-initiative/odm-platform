616875a0ea59887f153e2d31b4dc650b9fea57a4
Mattia Pennati <mattia.pennati@quantyca.it>
2024-04-30 13:51:21 +0200

    Mocks with mockito on policy server instead of using a PolicyEngineClientMock

--- product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/PolicyEngineProxy.java
+++ product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/services/proxies/PolicyEngineProxy.java
@@ -3,15 +3,31 @@ package org.opendatamesh.platform.pp.policy.server.services.proxies;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.opendatamesh.platform.pp.policy.api.resources.PolicyResource;
 import org.opendatamesh.platform.pp.policy.server.database.entities.PolicyEngine;
+import org.opendatamesh.platform.up.policy.api.v1.clients.PolicyEngineClient;
+import org.opendatamesh.platform.up.policy.api.v1.clients.PolicyEngineClientImpl;
+import org.opendatamesh.platform.up.policy.api.v1.resources.DocumentResource;
 import org.opendatamesh.platform.up.policy.api.v1.resources.EvaluationResource;
+import org.springframework.stereotype.Service;
 
-public interface PolicyEngineProxy {
+@Service
+public class PolicyEngineProxy {
 
-    EvaluationResource validatePolicy(
+    public EvaluationResource validatePolicy(
             Long policyEvaluationResultId,
             PolicyResource policyToEvaluate,
             PolicyEngine policyEngine,
             JsonNode objectToEvaluate
-    );
+    ) {
+        DocumentResource documentResource = new DocumentResource();
+        documentResource.setPolicy(policyToEvaluate);
+        documentResource.setPolicyEvaluationId(policyEvaluationResultId);
+        documentResource.setObjectToEvaluate(objectToEvaluate);
+        return getPolicyEngineClient(policyEngine).evaluateDocument(documentResource);
+    }
+
+    protected PolicyEngineClient getPolicyEngineClient(PolicyEngine policyEngine) {
+        PolicyEngineClient policyEngineClient = new PolicyEngineClientImpl(policyEngine.getAdapterUrl());
+        return policyEngineClient;
+    }
 
 }
--- product-plane-services/policy-server/src/test/java/org/opendatamesh/platform/pp/policy/server/ODMPolicyIT.java
+++ product-plane-services/policy-server/src/test/java/org/opendatamesh/platform/pp/policy/server/ODMPolicyIT.java
@@ -1,7 +1,10 @@
 package org.opendatamesh.platform.pp.policy.server;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.opendatamesh.platform.core.commons.test.ODMIntegrationTest;
 import org.opendatamesh.platform.core.commons.test.ODMResourceBuilder;
 import org.opendatamesh.platform.core.dpds.ObjectMapperFactory;
@@ -10,10 +13,14 @@ import org.opendatamesh.platform.pp.policy.api.resources.PolicyEngineResource;
 import org.opendatamesh.platform.pp.policy.api.resources.PolicyEvaluationRequestResource;
 import org.opendatamesh.platform.pp.policy.api.resources.PolicyEvaluationResultResource;
 import org.opendatamesh.platform.pp.policy.api.resources.PolicyResource;
+import org.opendatamesh.platform.pp.policy.server.database.entities.PolicyEngine;
+import org.opendatamesh.platform.pp.policy.server.services.proxies.PolicyEngineProxy;
+import org.opendatamesh.platform.up.policy.api.v1.resources.EvaluationResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -39,6 +46,9 @@ public class ODMPolicyIT extends ODMIntegrationTest {
 
     protected PolicyClientImpl policyClient;
 
+    @MockBean
+    protected PolicyEngineProxy policyEngineProxy;
+
     protected Logger logger = LoggerFactory.getLogger(ODMPolicyIT.class);
 
     protected final String DB_TABLES_POSTGRESQL = "src/test/resources/db/tables_postgresql.txt";
@@ -50,6 +60,17 @@ public class ODMPolicyIT extends ODMIntegrationTest {
         mapper = ObjectMapperFactory.JSON_MAPPER;
         resourceBuilder = new ODMResourceBuilder(mapper);
         policyClient = new PolicyClientImpl("http://localhost:" + port, mapper);
+    }
+
+    @Before
+    public void mockSetUp() {
+        // Mock communication with Policy Engines
+        EvaluationResource mockResponse = new EvaluationResource();
+        mockResponse.setPolicyEvaluationId(1L);
+        mockResponse.setEvaluationResult(true);
+        mockResponse.setOutputObject("{\"message\": \"OK\"}");
+        Mockito.when(policyEngineProxy.validatePolicy(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(mockResponse);
     }
 
     @BeforeEach

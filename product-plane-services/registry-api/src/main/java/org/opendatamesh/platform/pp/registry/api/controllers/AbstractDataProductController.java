01dda918475cc374328ecdf086c65112b1d62086
Mattia Pennati <mattia.pennati@quantyca.it>
2024-05-13 19:38:05 +0200

    renamings

--- product-plane-services/registry-api/src/main/java/org/opendatamesh/platform/pp/registry/api/controllers/AbstractDataProductController.java
+++ product-plane-services/registry-api/src/main/java/org/opendatamesh/platform/pp/registry/api/controllers/AbstractDataProductController.java
@@ -81,7 +81,7 @@ public abstract class AbstractDataProductController
             responseCode = "501", 
             description = "[Bad Gateway](https://www.rfc-editor.org/rfc/rfc9110.html#name-502-bad-gateway)"
             + "\r\n - Error Code 50201 - Invalid policyService's response" 
-            + "\r\n - Error Code 50204 - Invalid metaService's response",
+            + "\r\n - Error Code 50204 - Invalid notificationService's response",
             content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorRes.class))}
         )
     })

d7ff00eee64c70032cbc523376dac7d2fd1daa71
gabriele <gabriele.fantini@blindata.io>
2024-08-01 18:05:11 +0200

    Extended Policies search options + minor fixes on Policy Crud service

--- product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/database/repositories/PolicyRepository.java
+++ product-plane-services/policy-server/src/main/java/org/opendatamesh/platform/pp/policy/server/database/repositories/PolicyRepository.java
@@ -1,9 +1,10 @@
 package org.opendatamesh.platform.pp.policy.server.database.repositories;
 
-import org.opendatamesh.platform.pp.policy.server.database.entities.Policy;
-import org.opendatamesh.platform.pp.policy.server.database.entities.Policy_;
 import org.opendatamesh.platform.core.commons.database.utils.PagingAndSortingAndSpecificationExecutorRepository;
 import org.opendatamesh.platform.core.commons.database.utils.SpecsUtils;
+import org.opendatamesh.platform.pp.policy.server.database.entities.Policy;
+import org.opendatamesh.platform.pp.policy.server.database.entities.PolicyEngine_;
+import org.opendatamesh.platform.pp.policy.server.database.entities.Policy_;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface PolicyRepository extends PagingAndSortingAndSpecificationExecutorRepository<Policy, Long> {
@@ -21,12 +22,24 @@ public interface PolicyRepository extends PagingAndSortingAndSpecificationExecut
                     criteriaBuilder.equal(root.get(Policy_.isLastVersion), lastVersion)
             );
         }
+
         public static Specification<Policy> hasEvaluationEvent(String evaluationEvent) {
             return ((root, query, criteriaBuilder) ->
                     criteriaBuilder.equal(root.get(Policy_.evaluationEvent), evaluationEvent)
             );
         }
 
+        public static Specification<Policy> hasEngineName(String policyEngineName) {
+            return ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get(Policy_.policyEngine).get(PolicyEngine_.name), policyEngineName)
+            );
+        }
+
+        public static Specification<Policy> hasName(String name) {
+            return ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get(Policy_.name), name)
+            );
+        }
     }
 
 }

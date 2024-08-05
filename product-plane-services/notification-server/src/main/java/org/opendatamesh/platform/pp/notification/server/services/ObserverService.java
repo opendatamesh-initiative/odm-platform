57baf5bdc3fe4a059ce82de7d2b3dfd10ca13c67
gabriele <gabriele.fantini@blindata.io>
2024-08-05 14:29:30 +0200

    minor improvement to notification service subscription

--- product-plane-services/notification-server/src/main/java/org/opendatamesh/platform/pp/notification/server/services/ObserverService.java
+++ product-plane-services/notification-server/src/main/java/org/opendatamesh/platform/pp/notification/server/services/ObserverService.java
@@ -11,9 +11,12 @@ import org.opendatamesh.platform.pp.notification.server.database.mappers.Observe
 import org.opendatamesh.platform.pp.notification.server.database.repositories.ObserverRepository;
 import org.opendatamesh.platform.pp.notification.server.services.utils.GenericMappedAndFilteredCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import java.util.Optional;
 
 @Service
 public class ObserverService extends GenericMappedAndFilteredCrudService<ObserverSearchOptions, ObserverResource, Observer, Long> {
@@ -24,7 +27,8 @@ public class ObserverService extends GenericMappedAndFilteredCrudService<Observe
     @Autowired
     private ObserverMapper mapper;
 
-    protected ObserverService() {}
+    protected ObserverService() {
+    }
 
     @Override
     protected void validate(Observer objectToValidate) {
@@ -49,18 +53,9 @@ public class ObserverService extends GenericMappedAndFilteredCrudService<Observe
     }
 
     @Override
-    protected void beforeCreation(Observer observer) {
-        if (repository.existsByName(observer.getName())) {
-            throw new UnprocessableEntityException(
-                    NotificationApiStandardErrors.SC422_02_OBSERVER_ALREADY_EXISTS,
-                    "Observer with name [" + observer.getName() + "] already exists"
-            );
-        }
-    }
-
-    @Override
     protected void reconcile(Observer objectToReconcile) {
-        // No reconcile action needed
+        Optional<Observer> oldObserver = repository.findByName(objectToReconcile.getName(), Pageable.ofSize(1)).stream().findFirst();
+        oldObserver.ifPresent(observer -> objectToReconcile.setId(observer.getId()));
     }
 
     @Override

f2e290c142fc28bd232b209c6cb8cfff33cda08c
Jacopo Aliprandi <jacopo.aliprandi@blindata.io>
2025-09-30 16:19:17 +0200

    fix: remove redundant semicolons from imports across multiple files

--- product-plane-services/notification-api/src/main/java/org/opendatamesh/platform/pp/notification/api/clients/NotificationClientImpl.java
+++ product-plane-services/notification-api/src/main/java/org/opendatamesh/platform/pp/notification/api/clients/NotificationClientImpl.java
@@ -2,7 +2,7 @@ package org.opendatamesh.platform.pp.notification.api.clients;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.opendatamesh.platform.core.commons.ObjectMapperFactory;;
+import org.opendatamesh.platform.core.commons.ObjectMapperFactory;
 import org.opendatamesh.platform.core.commons.clients.ODMClient;
 import org.opendatamesh.platform.core.commons.clients.utils.RestUtils;
 import org.opendatamesh.platform.pp.notification.api.resources.*;
